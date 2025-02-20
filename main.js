b025c8b6189b02dbe0275198ebf3db1ff0177685
aanyk <aanykm@uw.edu>
2025-02-20 13:41:54 -0800

    changes

--- main.js
+++ main.js
@@ -3,60 +3,70 @@ const margin = { top: 50, right: 30, bottom: 60, left: 70 };
 const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-// Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
+// Create SVG container for the chart
+const svgLineChart = d3.select("#lineChart1") // Make sure this ID matches the one in index.html
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-const svg2_RENAME = d3.select("#lineChart2")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+// 2: LOAD CSV DATA
+d3.csv("aircraft_incidents.csv").then(data => {
+    console.log(data); // Check that the data loads properly.
 
-// (If applicable) Tooltip element for interactivity
-// const tooltip = ...
+    // 2.b: TRANSFORM DATA
+    data.forEach(d => {
+        d.Event_Date_Years = +d.Event_Date_Years;
+        d.Total_Fatal_Injuries = +d.Total_Fatal_Injuries;
+        d.Total_Serious_Injuries = +d.Total_Serious_Injuries;
+        d.Total_Uninjured = +d.Total_Uninjured;
+    });
 
-// 2.a: LOAD...
-d3.csv("YOUR_CSV_NAME.csv").then(data => {
-    // 2.b: ... AND TRANSFORM DATA
-
-    // 3.a: SET SCALES FOR CHART 1
-
-
-    // 4.a: PLOT DATA FOR CHART 1
-
-
-    // 5.a: ADD AXES FOR CHART 1
-
-
-    // 6.a: ADD LABELS FOR CHART 1
-
-
-    // 7.a: ADD INTERACTIVITY FOR CHART 1
+    // 3: SET SCALES FOR THE CHART
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.Event_Date_Years))
+        .range([0, width]);
     
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.Total_Fatal_Injuries)])
+        .range([height, 0]);
 
-    // ==========================================
-    //         CHART 2 (if applicable)
-    // ==========================================
+    // 4: PLOT DATA (a basic scatter plot)
+    svgLineChart.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.Event_Date_Years))
+        .attr("cy", d => yScale(d.Total_Fatal_Injuries))
+        .attr("r", 4)
+        .attr("fill", "steelblue");
 
-    // 3.b: SET SCALES FOR CHART 2
+    // 5: ADD AXES FOR THE CHART
+    // X-axis at the bottom
+    svgLineChart.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale).tickFormat(d3.format("d")));
+    
+    // Y-axis on the left
+    svgLineChart.append("g")
+        .call(d3.axisLeft(yScale));
 
+    // 6: ADD LABELS FOR THE CHART
+    // X-Axis Label
+    svgLineChart.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .attr("text-anchor", "middle")
+        .text("Year of Incident");
+    
+    // Y-Axis Label
+    svgLineChart.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -height / 2)
+        .attr("text-anchor", "middle")
+        .text("Total Fatal Injuries");
 
-    // 4.b: PLOT DATA FOR CHART 2
-
-
-    // 5.b: ADD AXES FOR CHART 
-
-
-    // 6.b: ADD LABELS FOR CHART 2
-
-
-    // 7.b: ADD INTERACTIVITY FOR CHART 2
-
-
+    // 7: ADD INTERACTIVITY (Optional tooltip code can be added here)
 });
